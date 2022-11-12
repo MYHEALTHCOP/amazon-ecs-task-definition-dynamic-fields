a66d6d093a0bfe008548e63f53cb4bb95167ffba
QClaire <38317525+Qclaire@users.noreply.github.com>
2022-11-12 21:42:14 +0000

    testing inputs

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
 
 family = os.environ.get('INPUT_FAMILY-NAME')
+output = os.environ.get("GITHUB_OUTPUT")
 
 print(f'::set-output name=task-definition::${family}')
+print(f'$output')
 
 
 def main():
     print(f"\n\nHello GitHub actions: ${family}\n\n")
-    print(f'::set-output name=task-definition::${family}')
+    print(f'::set-output name=task-definition::${}')
 
 if __name__ == "__main__":
     main()
