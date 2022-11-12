6d5bc4762869033b36f359c12bea298a29330246
QClaire <38317525+Qclaire@users.noreply.github.com>
2022-11-12 22:16:50 +0000

    tesing outputs

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 family = os.environ.get('INPUT_FAMILY-NAME')
 output = os.environ.get("GITHUB_OUTPUT")
 
-print(f'::set-output name=task-definition::${family}')
+print(f'::setOutput name=task-definition::${family}')
 
 
 
