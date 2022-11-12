302dcc604c3f9394fecb69b2034a13bf39b97818
QClaire <38317525+Qclaire@users.noreply.github.com>
2022-11-12 20:54:13 +0000

    tesing outputs

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ print(f'::set-output name=task-definition::${family}')
 
 def main():
     print(f"\n\nHello GitHub actions: ${family}\n\n")
+    print(f'::set-output name=task-definition::${family}')
 
 if __name__ == "__main__":
     main()
