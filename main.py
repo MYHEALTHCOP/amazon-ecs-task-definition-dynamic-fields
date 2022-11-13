91ba5b131da9918d510811a7bae77149037dbeef
QClaire <38317525+Qclaire@users.noreply.github.com>
2022-11-13 00:45:28 +0000

    testing downloading taskdef with non-existing  version

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ class TaskDefinitionConfig:
         self.region = os.environ.get('INPUT_AWS-REGION')
         self.access_key_id = os.environ.get('INPUT_AWS-ACCESS-KEY-ID')
         self.secret_access_key = os.environ.get('INPUT_AWS-SECRET-ACCESS-KEY')
+        self.fields = os.environ.get('INPUT_DYNAMIC-FIELDS')
         self.config = Config(region_name=self.region, signature_version='v4', retries={'max_attempts': 3, 'mode': 'standard'})
         self.ecs = boto3.client('ecs', aws_access_key_id=self.access_key_id, aws_secret_access_key=self.secret_access_key,config=self.config)
 
     def download_latest_revision(self):
-        return self.ecs.describe_task_definition(taskDefinition='{}:17'.format(self.family))
+        return self.ecs.describe_task_definition(taskDefinition='{}:1'.format(self.family))
     
     def fill_in_given_fields(self):
         pass
