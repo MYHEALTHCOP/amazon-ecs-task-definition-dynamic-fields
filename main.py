f6e455e83f8d9a278dae19a58c731226eb59c712
QClaire <38317525+Qclaire@users.noreply.github.com>
2022-11-14 01:41:44 +0000

    setting family param

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class TaskDefinitionConfig:
         self.task_definition.pop("registeredAt", None)
         self.task_definition.pop("deregisteredAt", None)
         try:
-            response = self.ecs.register_task_definition(containerDefinitions=self.task_definition['containerDefinitions'])
+            response = self.ecs.register_task_definition(containerDefinitions=self.task_definition['containerDefinitions'], family=self.family)
         except Exception as error:
             raise error
         else:
