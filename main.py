178a3e382a3e537464adf4cfed860674d8ce2f6b
QClaire <38317525+Qclaire@users.noreply.github.com>
2022-11-14 00:55:36 +0000

    downloading new version

--- main.py
+++ main.py
@@ -11,33 +11,42 @@ class TaskDefinitionConfig:
         self.region = os.environ.get('INPUT_AWS-REGION')
         self.access_key_id = os.environ.get('INPUT_AWS-ACCESS-KEY-ID')
         self.secret_access_key = os.environ.get('INPUT_AWS-SECRET-ACCESS-KEY')
-        self.fields = os.environ.get('INPUT_DYNAMIC-FIELDS')
+        self.image = os.environ.get('INPUT_IMAGE-URL')
         self.config = Config(region_name=self.region, signature_version='v4', retries={'max_attempts': 3, 'mode': 'standard'})
         self.ecs = boto3.client('ecs', aws_access_key_id=self.access_key_id, aws_secret_access_key=self.secret_access_key,config=self.config)
-
-    def download_latest_revision(self):
-        return self.ecs.describe_task_definition(taskDefinition='{}:{}'.format(self.family, self.revision))
+        self.task_definition = None
+        self.updated_task_definition = None
     
-    def fill_in_given_fields(self):
-        for key, value in self.fields:
-            if key
-            pass
+    def download_task_definition(self):
+        try:
+            response = self.ecs.describe_task_definition(taskDefinition='{}:{}'.format(self.family, self.revision))
+        except Exception as e:
+            raise Exception(str(e))
+        else:
+            meta_data = response.pop('ResponseMetadata')
+            if meta_data['HTTPStatusCode'] == 200:
+                print(response)
+                self.task_definition = response['taskDefinition']
+                          
+
+    def replace_image_uri(self):
+        self.task_definition['containerDefinitions'][0]['image'] = self.image
+         
+       
 
     def save_new_task_definition(self):
-        pass
-    
-    def credentials(self):
-        return {
-            "access_key_id": self.access_key_id,
-            "region": self.region,
-            "family": self.family,
-            "secret_access_key": self.secret_access_key
-        }
-
-
-    
-    
+        del self.task_definition["registeredAt"]
+        del self.task_definition["deregisteredAt"]
+        try:
+            response = self.ecs.register_task_definition(**self.task_definition)
+        except Exception as e:
+            raise Exception(str(e))
+        else:
+            self.updated_task_definition = response['taskDefinition']
+            print(response)   
 
 if __name__ == "__main__":
-    print(TaskDefinitionConfig().download_latest_revision())
-    print(TaskDefinitionConfig().credentials())
+    task_definition_config = TaskDefinitionConfig()
+    task_def = task_definition_config.download_task_definition()
+    # task_definition_config.replace_image_uri()
+    # task_definition_config.save_new_task_definition()
