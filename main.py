c6670639f10d132f6f2ca4af2237e7f79b55c1f5
QClaire <38317525+Qclaire@users.noreply.github.com>
2022-11-14 21:14:32 +0000

    setting revision

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class TaskDefinitionConfig:
     def __init__(self):
         self.family = os.environ.get('INPUT_TASK-FAMILY')
         self.revision = os.environ.get('INPUT_TASK-REVISION')
+        self.new_revision = self.revision
         self.region = os.environ.get('INPUT_AWS-REGION')
         self.access_key_id = os.environ.get('INPUT_AWS-ACCESS-KEY-ID')
         self.secret_access_key = os.environ.get('INPUT_AWS-SECRET-ACCESS-KEY')
@@ -65,8 +66,6 @@ class TaskDefinitionConfig:
             
             if meta_data['HTTPStatusCode'] == 200:
                 self.task_definition = self.purge_useless_keys(response['taskDefinition'])
-                logger.info(self.task_definition.get('requiresCompatibilities'))
-                logger.info(self.task_definition.get('compatibilities'))
                 self.revision = self.task_definition['revision']
                 self.task_role_arn = self.task_definition.get('taskRoleArn')
                 self.execution_role_arn = self.task_definition.get('executionRoleArn')
@@ -82,19 +81,19 @@ class TaskDefinitionConfig:
 
     def fill_in_required_info(self):
         self.container_definitions[0]['image'] = self.image
-        self.task_definition['requiresCompatibilities'] = ['FARGATE']
+        # self.task_definition['requiresCompatibilities'] = ['FARGATE']
         logger.info('Image URI and other info updated!')
          
        
 
     def save_new_task_definition(self):
         try:
-            logger.info(self.container_definitions[0].get("image"))
             response = self.ecs.register_task_definition(containerDefinitions=self.container_definitions, family=self.family, executionRoleArn=self.execution_role_arn, taskRoleArn=self.task_role_arn)
         except Exception as error:
             raise error
         else:
-            logger.info('Sucess: %s --> %s:%d', self.task_definition_name, self.family,int(self.revision)+1)
+            self.new_revision = response['taskDefinition']['revision']
+            logger.info('Sucess: %s:%d --> %s:%d', self.family, self.revision, self.family, self.new_revision)
     
     def update_ecs_service(self):
         try:
